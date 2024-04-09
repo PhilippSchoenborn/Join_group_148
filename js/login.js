29d03852208adf5c3edcac738759ab88bb54097a
Julia Schäffer <juliaschaeffer96@gmail.com>
2024-04-09 17:27:09 +0200

    code architecture

--- js/login.js
+++ js/login.js
@@ -7,27 +7,27 @@ async function init() {
 async function loadUsers() {
     try {
         users = JSON.parse(await getItem('users'));
-    } catch(e){
+    } catch (e) {
         console.warn('could not load users');
     }
 }
 
 
-    async function register() {
-        document.getElementById('registerBtn').disabled = true;
-        users.push({
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value,
-        })
+async function register() {
+    document.getElementById('registerBtn').disabled = true;
+    users.push({
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+    })
 
-        await setItem('users', JSON.stringify(users));
-        resetForm();
-    }
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+}
 
-    function resetForm() {
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('password').value = '';
-        document.getElementById('registerBtn').disabled = false;
-    }
+function resetForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('registerBtn').disabled = false;
+}
