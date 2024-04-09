33c22e34438ae5080eaa91af329c9cd90840b84b
Julia Schäffer <juliaschaeffer96@gmail.com>
2024-04-09 17:26:12 +0200

    login commit

---
+++ js/login.js
@@ -0,0 +1,33 @@
+let users = [];
+
+async function init() {
+    loadUsers();
+}
+
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.warn('could not load users');
+    }
+}
+
+
+    async function register() {
+        document.getElementById('registerBtn').disabled = true;
+        users.push({
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+        })
+
+        await setItem('users', JSON.stringify(users));
+        resetForm();
+    }
+
+    function resetForm() {
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('registerBtn').disabled = false;
+    }
