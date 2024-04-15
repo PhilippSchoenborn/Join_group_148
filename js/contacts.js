0f0dcd8aab46c969cccfaf6f634a7932d736f3fe
Julia Schäffer <juliaschaeffer96@gmail.com>
2024-04-15 19:05:28 +0200

    contacts blur background

--- js/contacts.js
+++ js/contacts.js
@@ -1,10 +1,12 @@
 function cancelAddContact() {
     document.getElementById('addNewContact').classList.remove('addnewContactActive');
+    document.getElementById('blurBackground').classList.add('d-none');
 }
 
 
 function showAddContact() {
     document.getElementById('addNewContact').classList.add('addnewContactActive');
+    document.getElementById('blurBackground').classList.remove('d-none');
 }
 
 function writeContact(event) {
