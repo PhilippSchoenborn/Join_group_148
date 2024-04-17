d31b6711fc2f62f517349284061ad0d99ab08cec
Julia Schäffer <juliaschaeffer96@gmail.com>
2024-04-17 20:37:01 +0200

    push contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,43 @@
+let contacts = [
+    {
+        "name": "Julia Schäffer",
+        "email": "julia.sch@hotmail.de",
+        "phone": "+491778965144"
+    },
+    {
+        "name": "Phillip Schönborn",
+        "email": "philipp@schoenborn-home.de",
+        "phone": "+49438721221"
+    },
+    {
+        "name": "Tobias Mueller",
+        "email": "webstor21@gmail.com",
+        "phone": "+49152341609"
+    },
+    {
+        "name": "Nathalie Strauchmann",
+        "email": "strauchmann89@yahoo.de",
+        "phone": "+49151338395"
+    },
+    {
+        "name": "Melanie Müller",
+        "email": "m.muellerstreich@gmail.com",
+        "phone": "+491760152757"
+    },
+    {
+        "name": "Herbert Peter",
+        "email": "Petermann@hotmail.de",
+        "phone": "+491723687234"
+    },
+    {
+        "name": "Heiko Lee",
+        "email": "h.lee99@home.com",
+        "phone": ""
+    },
+
+];
+
+// Schließt die Box 'Add new Contact'
 function cancelAddContact() {
     document.getElementById('addNewContact').classList.remove('addnewContactActive');
     document.getElementById('blurBackground').classList.add('d-none');
@@ -5,16 +45,19 @@ function cancelAddContact() {
     document.getElementById('addContactButton').classList.remove('buttonActive');
 }
 
+// Schließt die Box 'Edit Contact'
 function cancelEditContact(){
     document.getElementById('editContact').classList.remove('editContactActive');
     document.getElementById('blurBackground').classList.add('d-none');
 } 
 
+// Öffnet die Box 'Edit Contact'
 function showeditContact() {
     document.getElementById('editContact').classList.add('editContactActive');
     document.getElementById('blurBackground').classList.remove('d-none');
 }
 
+// Öffnet die Box 'Add new Contact'
 function showAddContact() {
     document.getElementById('addNewContact').classList.add('addnewContactActive');
     document.getElementById('blurBackground').classList.remove('d-none');
@@ -22,10 +65,12 @@ function showAddContact() {
     document.getElementById('addContactButton').classList.add('buttonActive');
 }
 
+// fügt der InputBox den Effekt 'blaue Linie' hinzu
 function writeContact(event) {
     document.getElementById('inputBox1').classList.add('blueborder');
 }
 
+// Entfernt der InputBox den Effekt 'blaue Linie' 
 function StopPropergation() {
     document.getElementById('inputBox1').classList.remove('blueborder');
 } 
