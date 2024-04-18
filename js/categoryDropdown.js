317c91f2968e17fd6d720d8fa681906b076f96c8
PhilippSchoenborn <philipp@schoenborn-home.de>
2024-04-18 09:03:25 +0200

    add fromAddTAsk

--- js/categoryDropdown.js
+++ js/categoryDropdown.js
@@ -9,7 +9,6 @@ function closeCategoriesDropdown() {
     dropdownCategoriesList.classList.remove("show");
 }
 
-// Öffnen oder Schließen des Kategorien-Dropdown-Menüs beim Klicken auf das Eingabefeld
 categoryInput.onclick = function(e) {
     e.stopPropagation();
     if (!dropdownCategoriesList.classList.contains("show")) {
@@ -19,26 +18,22 @@ categoryInput.onclick = function(e) {
     }
 };
 
-// Schließen des Kategorien-Dropdown-Menüs, wenn außerhalb des Dropdowns geklickt wird
 window.onclick = function(event) {
     closeCategoriesDropdown();
 };
 
-// Verhindern, dass Klicks innerhalb des Dropdown-Menüs das Schließen des Menüs auslösen
 dropdownCategoriesList.onclick = function(event) {
     event.stopPropagation();
 };
 
-// onclick-Handler für Klicks auf die Dropdown-Liste der Kategorien
 dropdownCategoriesList.querySelectorAll('.dropdown-item').forEach(item => {
     item.onclick = function() {
         console.log("Selected category:", item.innerHTML.trim());
-        categoryInput.value = item.innerHTML.trim(); // Kategorie im Eingabefeld anzeigen
-        closeCategoriesDropdown(); // Dropdown-Menü schließen
+        categoryInput.value = item.innerHTML.trim();
+        closeCategoriesDropdown();
     };
 });
 
-// Funktion zum Öffnen des Kategorien-Dropdown-Menüs und Schließen des Kontakte-Dropdown-Menüs
 function openCategoriesAndCloseContactsDropdown() {
     openCategoriesDropdown();
     closeContactsDropdown();
@@ -49,14 +44,11 @@ categoryInput.previousElementSibling.onclick = function(e) {
     e.stopPropagation();
 };
 
-
 document.getElementById('categoryDropdown').onclick = function(e) {
     openCategoriesAndCloseContactsDropdown();
     e.stopPropagation();
 };
 
-
-// Klick-Handler für die Dropdown-Liste der Kategorien, um ausgewählte Kategorien zu aktualisieren
 dropdownCategoriesList.querySelectorAll('.dropdown-item').forEach(item => {
     item.onclick = function(e) {
         updateSelectedCategory(item);
